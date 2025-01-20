732ea4cecc49a0a421c93b4708eea37ea807fa2b
admin <admin@admindeMacBook-Air.local>
2025-01-20 22:56:56 +0800

    i have modify the tg

--- dexprice/modules/tg/test/test_tgbot.py
+++ dexprice/modules/tg/test/test_tgbot.py
@@ -1,4 +1,4 @@
 import dexprice.modules.tg.tgbot as tgbot
 
 if __name__ == '__main__':
-    tgbot.sendmessage("6rvir3c4H9cvMxtz38aG9TJPgH1sDUiGpUnupiHituVs")
+    tgbot.sendmessage("6rvir3c4H9cvMxtz38aG9TJPgH1sDUiGpUnupiHituVs",7890)
--- dexprice/modules/tg/tgbot.py
+++ dexprice/modules/tg/tgbot.py
@@ -6,7 +6,7 @@ import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
-def send_message(token, chat_id, text):
+def send_message(token, chat_id, text,port):
     """
     发送带有 MarkdownV2 格式的消息，使 address 显示为固定宽度字体，并具有重试机制。
 
@@ -24,8 +24,8 @@ def send_message(token, chat_id, text):
 
     # 定义HTTP代理
     proxies = {
-        'http': 'http://127.0.0.1:7890',
-        'https': 'http://127.0.0.1:7890'
+        'http': 'http://127.0.0.1:'+str(port),
+        'https': 'http://127.0.0.1:'+str(port)
     }
 
     session = requests.Session()
@@ -50,7 +50,7 @@ def send_message(token, chat_id, text):
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
         return f"Error: {e}"
-def sendmessage(address):
+def sendmessage(address,port):
     escaped_text = escape_markdown(address)
 
     link = f"https://dexscreener.com/solana/{address}"
@@ -61,7 +61,7 @@ def sendmessage(address):
     )
 
     token = "7190422738:AAEuhELn2QcVfEDa-cF4bAwe4KgyQmbNF_Q"
-    send_message(token, "@jingou11", message_content)
+    send_message(token, "@jingou11", message_content,port)
 def escape_markdown(text):
     """
     转义 MarkdownV2 中的特殊字符
