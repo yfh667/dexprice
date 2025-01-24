e9cba64e67d395baadf6e06c71dc7f03ba261964
admin <admin@admindeMacBook-Air.local>
2025-01-24 20:31:36 +0800

    we have modify the clash proxy

--- dexprice/modules/proxy/test/test_clashapi.py
+++ dexprice/modules/proxy/test/test_clashapi.py
@@ -7,11 +7,11 @@ if __name__ == "__main__":
     clash_api_url = "http://127.0.0.1:9097"
     headers = {"Authorization": "Bearer 123"}
     proxies = clash.get_all_proxies(clash_api_url,headers)
-
-    proxynumber = clash.get_proxy_number(proxies)
-    available_ports = getproxy.check_open_ports(50000, proxynumber)
-
-    print("可用端口:", available_ports)
+    #
+    # proxynumber = clash.get_proxy_number(proxies)
+    # available_ports = getproxy.check_open_ports(50000, proxynumber)
+    #
+    # print("可用端口:", available_ports)
     proxys =clash.get_one_ip_proxy(50000,clash_api_url,headers)
     print(proxys)
 
